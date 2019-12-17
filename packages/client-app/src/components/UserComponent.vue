b701565bd4e90bdc71862d3cb57d3c4ac8a87234
Remi Thebault <remi.thebault@gmail.com>
2019-12-17 22:30:47 +0100

    user component

---
+++ packages/client-app/src/components/UserComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-card>
+        <v-card-title>
+            {{ user.fullName }}
+        </v-card-title>
+        <v-card-subtitle>
+            {{ roleString }}
+        </v-card-subtitle>
+        <v-card-text>
+            <v-text-field v-model="user.name" label="Login"></v-text-field>
+            <v-text-field v-model="user.email" label="E-Mail"></v-text-field>
+        </v-card-text>
+    </v-card>
+</template>
+
+<script>
+import { CUser } from '@engspace/core';
+export default {
+    props: {
+        user: {
+            type: Object,
+            default: new CUser(),
+        },
+    },
+    computed: {
+        roleString() {
+            return this.user.roles.map(r => r.charAt(0).toUpperCase() + r.slice(1)).join(', ');
+        },
+    },
+};
+</script>
