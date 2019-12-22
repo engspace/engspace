9706c20297f3508621e97bd76733cb8dbd888250
Remi Thebault <remi.thebault@gmail.com>
2019-12-22 13:09:32 +0100

    core not doing a blind clone

--- packages/core/src/ctor.ts
+++ packages/core/src/ctor.ts
@@ -2,12 +2,16 @@ import { Id, Role, User } from './schema';
 
 export class CUser implements User {
     id: Id;
-    name = '';
-    email = '';
-    fullName = '';
-    roles: Array<Role> = [];
+    name: string;
+    email: string;
+    fullName: string;
+    roles: Array<Role>;
 
     constructor(user: Partial<User> = {}) {
-        Object.assign(this, user);
+        this.id = user.id ?? '';
+        this.name = user.name ?? '';
+        this.email = user.email ?? '';
+        this.fullName = user.fullName ?? '';
+        this.roles = user.roles ?? [];
     }
 }
