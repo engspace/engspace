86c91fc8af2dacb005dda802a9460265dc6f3076
Remi Thebault <remi.thebault@gmail.com>
2019-12-29 21:50:04 +0100

    extractGqlError

--- packages/client-app/src/apollo.ts
+++ packages/client-app/src/apollo.ts
@@ -1,7 +1,9 @@
 import { InMemoryCache } from 'apollo-cache-inmemory';
-import { ApolloClient } from 'apollo-client';
+import { ApolloClient, ApolloError } from 'apollo-client';
 import { ApolloLink } from 'apollo-link';
 import { createHttpLink } from 'apollo-link-http';
+import { GraphQLError } from 'graphql';
+import get from 'lodash.get';
 import Vue from 'vue';
 import VueApollo from 'vue-apollo';
 import store from './store';
@@ -39,3 +41,22 @@ Vue.use(VueApollo);
 export const apolloProvider = new VueApollo({
     defaultClient: apolloClient,
 });
+
+export function extractGQLError(err: ApolloError): GraphQLError {
+    const errorPaths = [
+        'graphQLErrors[0].message',
+        'networkError.result.errors[0].message',
+        'response.errors[0].message',
+        'message',
+    ];
+    const path = errorPaths.find(path => get(err, path)) as string;
+    return get(err, path);
+}
+
+export function extractGQLErrors(err: ApolloError): GraphQLError[] {
+    const errors: GraphQLError[] = [];
+    const errorPaths = ['graphQLErrors', 'networkError.result.errors', 'response.errors'];
+    const paths = errorPaths.filter(path => get(err, path));
+    paths.forEach(path => errors.push(...get(err, path)));
+    return errors;
+}
