a16685eaf9e464151b812ebee530b2a77274de4c
Remi Thebault <remi.thebault@gmail.com>
2020-02-14 23:15:31 +0100

    testing api project

--- packages/server-api/test/project.ts
+++ packages/server-api/test/project.ts
@@ -39,7 +39,37 @@ const PROJECT_READ = gql`
 
 const PROJECT_READ_BYCODE = gql`
     query ReadProjectByCode($code: String!) {
-        projectByCode(code: $String) {
+        projectByCode(code: $code) {
+            ...ProjectFields
+        }
+    }
+    ${PROJECT_FIELDS}
+`;
+
+const PROJECT_SEARCH = gql`
+    query SeachProject($search: String) {
+        projectSearch(search: $search) {
+            count
+            projects {
+                ...ProjectFields
+            }
+        }
+    }
+    ${PROJECT_FIELDS}
+`;
+
+const PROJECT_CREATE = gql`
+    mutation CreateProject($project: ProjectInput!) {
+        projectCreate(project: $project) {
+            ...ProjectFields
+        }
+    }
+    ${PROJECT_FIELDS}
+`;
+
+const PROJECT_UPDATE = gql`
+    mutation UpdateProject($id: ID!, $project: ProjectInput!) {
+        projectUpdate(id: $id, project: $project) {
             ...ProjectFields
         }
     }
@@ -103,7 +133,7 @@ describe('GraphQL Project', () => {
             });
             expect(result.errors).to.be.undefined;
             expect(result.data).to.be.an('object');
-            expect(result.data.projectByCode).to.deep.include(projects.chair);
+            expect(result.data.projectByCode).to.deep.include(projects.desk);
         });
 
         it('should not read project by code without "project.read"', async () => {
@@ -119,6 +149,144 @@ describe('GraphQL Project', () => {
                 .with.lengthOf.at.least(1);
             expect(result.data).to.be.an('object');
             expect(result.data.projectByCode).to.be.null;
+        });
+
+        it('should search project with "project.read"', async () => {
+            const result = await pool.connect(async db => {
+                const { query } = buildGqlServer(db, permsAuth(users.tania, ['project.read']));
+                return query({
+                    query: PROJECT_SEARCH,
+                });
+            });
+            expect(result.errors).to.be.undefined;
+            expect(result.data).to.be.an('object');
+            expect(result.data.projectSearch).to.eql({
+                count: 2,
+                projects: [projects.chair, projects.desk],
+            });
+        });
+
+        it('should not search project without "project.read"', async () => {
+            const result = await pool.connect(async db => {
+                const { query } = buildGqlServer(db, permsAuth(users.tania, []));
+                return query({
+                    query: PROJECT_SEARCH,
+                });
+            });
+            expect(result.errors)
+                .to.be.an('array')
+                .with.lengthOf.at.least(1);
+            expect(result.data).to.be.null;
+        });
+
+        it('should search project by code', async () => {
+            const result = await pool.connect(async db => {
+                const { query } = buildGqlServer(db, permsAuth(users.tania, ['project.read']));
+                return query({
+                    query: PROJECT_SEARCH,
+                    variables: { search: 'chair' },
+                });
+            });
+            expect(result.errors).to.be.undefined;
+            expect(result.data).to.be.an('object');
+            expect(result.data.projectSearch).to.eql({
+                count: 1,
+                projects: [projects.chair],
+            });
+        });
+    });
+
+    describe('Mutation', () => {
+        describe('Create', () => {
+            afterEach(deleteAllProjects);
+
+            it('should create project with "project.create"', async function() {
+                const { errors, data } = await pool.transaction(async db => {
+                    const { mutate } = buildGqlServer(
+                        db,
+                        permsAuth(users.tania, ['project.create'])
+                    );
+                    return mutate({
+                        mutation: PROJECT_CREATE,
+                        variables: {
+                            project: projectsInput.chair,
+                        },
+                    });
+                });
+                expect(errors).to.be.undefined;
+                expect(data).to.be.an('object');
+                expect(data.projectCreate).to.deep.include(projectsInput.chair);
+                expect(data.projectCreate.id).to.be.a('string');
+            });
+            it('should not create project without "project.create"', async function() {
+                const { errors, data } = await pool.transaction(async db => {
+                    const { mutate } = buildGqlServer(db, permsAuth(users.tania, []));
+                    return mutate({
+                        mutation: PROJECT_CREATE,
+                        variables: {
+                            project: projectsInput.chair,
+                        },
+                    });
+                });
+                expect(errors)
+                    .to.be.an('array')
+                    .with.lengthOf.at.least(1);
+                expect(data).to.be.null;
+            });
+        });
+
+        describe('Update', function() {
+            let projects: DemoProjectSet;
+
+            before('Create projects', async () => {
+                projects = await pool.transaction(db => createProjects(db, projectsInput));
+            });
+
+            after(deleteAllProjects);
+
+            const mars = {
+                code: 'mars',
+                name: 'Mars',
+                description: 'A journey to Mars',
+            };
+
+            it('should update project with "project.update"', async function() {
+                const { errors, data } = await pool.transaction(async db => {
+                    const { mutate } = buildGqlServer(
+                        db,
+                        permsAuth(users.tania, ['project.update'])
+                    );
+                    return mutate({
+                        mutation: PROJECT_UPDATE,
+                        variables: {
+                            id: projects.chair.id,
+                            project: mars,
+                        },
+                    });
+                });
+                expect(errors).to.be.undefined;
+                expect(data).to.be.an('object');
+                expect(data.projectUpdate).to.deep.include({
+                    ...mars,
+                    id: projects.chair.id,
+                });
+            });
+            it('should not update project without "project.update"', async function() {
+                const { errors, data } = await pool.transaction(async db => {
+                    const { mutate } = buildGqlServer(db, permsAuth(users.tania, []));
+                    return mutate({
+                        mutation: PROJECT_UPDATE,
+                        variables: {
+                            id: projects.chair.id,
+                            project: mars,
+                        },
+                    });
+                });
+                expect(errors)
+                    .to.be.an('array')
+                    .with.lengthOf.at.least(1);
+                expect(data).to.be.null;
+            });
         });
     });
 });
