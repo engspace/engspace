c355970daa3377f3560f333c7b2177626000164f
Remi Thebault <remi.thebault@gmail.com>
2020-02-24 23:48:08 +0100

    combining nyc reports

---
+++ merge_nyc_reports.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+'use strict';
+
+// script from IstanbulJS
+
+const path = require('path');
+const { spawnSync } = require('child_process');
+
+const rimraf = require('rimraf');
+const makeDir = require('make-dir');
+const glob = require('glob');
+
+process.chdir(__dirname);
+rimraf.sync('.nyc_output');
+makeDir.sync('.nyc_output');
+// Merge coverage data from each package so we can generate a complete report
+glob.sync('packages/*/.nyc_output').forEach(nycOutput => {
+    const cwd = path.dirname(nycOutput);
+    console.log(`merging nyc output of ${path.basename(cwd)}`);
+    const { status, stderr } = spawnSync(
+        'nyc',
+        [
+            'merge',
+            '.nyc_output',
+            path.join(__dirname, '.nyc_output', path.basename(cwd) + '.json')
+        ],
+        {
+            encoding: 'utf8',
+            shell: true,
+            cwd
+        }
+    );
+
+    if (status !== 0) {
+        console.error(stderr);
+        process.exit(status);
+    }
+});
