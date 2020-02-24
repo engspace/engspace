0dcb8400560300be9c813ff2ecdb1ffd88b0fae1
Remi Thebault <remi.thebault@gmail.com>
2020-02-24 11:44:40 +0100

    test project members gql mutations

--- packages/server-api/test/member.ts
+++ packages/server-api/test/member.ts
@@ -6,7 +6,7 @@ import {
     prepareProjects,
     prepareUsers,
 } from '@engspace/demo-data-input';
-import { memberDao, userDao, projectDao } from '@engspace/server-db';
+import { memberDao, projectDao, userDao } from '@engspace/server-db';
 import { expect } from 'chai';
 import gql from 'graphql-tag';
 import { buildGqlServer, pool } from '.';
@@ -84,19 +84,55 @@ const MEMBER_READ_BYUSER = gql`
     ${MEMBER_FIELDS}
 `;
 
+const MEMBER_CREATE = gql`
+    mutation CreateMember($member: ProjectMemberInput!) {
+        projectMemberCreate(projectMember: $member) {
+            ...MemberFields
+        }
+    }
+    ${MEMBER_FIELDS}
+`;
+
+const MEMBER_UPDATE = gql`
+    mutation UpdateMember($id: ID!, $roles: [String!]!) {
+        projectMemberUpdateRoles(id: $id, roles: $roles) {
+            ...MemberFields
+        }
+    }
+    ${MEMBER_FIELDS}
+`;
+
+const MEMBER_DELETE = gql`
+    mutation DeleteMember($id: ID!) {
+        projectMemberDelete(id: $id) {
+            ...MemberFields
+        }
+    }
+    ${MEMBER_FIELDS}
+`;
+
 describe('GraphQL members', function() {
-    const usersInput = prepareUsers();
-    const projectsInput = prepareProjects();
+    let users;
+    let projects;
+    before('Create users and projects', async function() {
+        return pool.transaction(async db => {
+            users = await createUsers(db, prepareUsers());
+            projects = await createProjects(db, prepareProjects());
+        });
+    });
+
+    after('Delete users and projects', async function() {
+        return pool.transaction(async db => {
+            await projectDao.deleteAll(db);
+            await userDao.deleteAll(db);
+        });
+    });
 
     describe('Query', function() {
-        let projects;
-        let users;
         let members;
 
-        before('Create users, projects and members', async function() {
+        before('Create members', async function() {
             return pool.transaction(async db => {
-                users = await createUsers(db, usersInput);
-                projects = await createProjects(db, projectsInput);
                 members = await createMembers(
                     db,
                     Promise.resolve(projects),
@@ -105,11 +141,9 @@ describe('GraphQL members', function() {
             });
         });
 
-        after('Delete users, projects and members', async function() {
+        after('Delete members', async function() {
             return pool.transaction(async db => {
                 await memberDao.deleteAll(db);
-                await projectDao.deleteAll(db);
-                await userDao.deleteAll(db);
             });
         });
 
@@ -265,6 +299,177 @@ describe('GraphQL members', function() {
                 .to.be.an('array')
                 .with.lengthOf.at.least(1);
             expect(data.user).to.be.null;
+        });
+    });
+
+    describe('Mutation', function() {
+        afterEach('Delete members', async function() {
+            return pool.transaction(async db => {
+                await memberDao.deleteAll(db);
+            });
+        });
+
+        describe('Create', function() {
+            it('should create a member with "member.create"', async function() {
+                const { errors, data } = await pool.transaction(async db => {
+                    const { mutate } = buildGqlServer(
+                        db,
+                        permsAuth(users.gerard, [
+                            'member.create',
+                            'member.read',
+                            'project.read',
+                            'user.read',
+                        ])
+                    );
+                    return mutate({
+                        mutation: MEMBER_CREATE,
+                        variables: {
+                            member: {
+                                projectId: projects.desk.id,
+                                userId: users.alphonse.id,
+                                roles: ['user', 'designer', 'some other role'],
+                            },
+                        },
+                    });
+                });
+                expect(errors).to.be.undefined;
+                expect(data).to.be.an('object');
+                expect(data.projectMemberCreate.id).to.be.uuid();
+                expect(data.projectMemberCreate).to.deep.include({
+                    project: { id: projects.desk.id },
+                    user: { id: users.alphonse.id },
+                });
+                expect(data.projectMemberCreate.roles).to.have.members([
+                    'user',
+                    'some other role',
+                    'designer',
+                ]);
+            });
+            it('should not create a member without "member.create"', async function() {
+                const { errors, data } = await pool.transaction(async db => {
+                    const { mutate } = buildGqlServer(
+                        db,
+                        permsAuth(users.gerard, ['member.read', 'project.read', 'user.read'])
+                    );
+                    return mutate({
+                        mutation: MEMBER_CREATE,
+                        variables: {
+                            member: {
+                                projectId: projects.desk.id,
+                                userId: users.alphonse.id,
+                                roles: ['user', 'designer', 'some other role'],
+                            },
+                        },
+                    });
+                });
+                expect(errors)
+                    .to.be.an('array')
+                    .with.lengthOf.at.least(1);
+                expect(data).to.be.null;
+            });
+        });
+
+        describe('Update', function() {
+            it('should update member roles with "member.update"', async function() {
+                const { memId, errors, data } = await pool.transaction(async db => {
+                    const m = await memberDao.create(db, {
+                        projectId: projects.desk.id,
+                        userId: users.alphonse.id,
+                        roles: ['user', 'designer', 'some other role'],
+                    });
+                    const { mutate } = buildGqlServer(
+                        db,
+                        permsAuth(users.gerard, [
+                            'member.update',
+                            'member.read',
+                            'project.read',
+                            'user.read',
+                        ])
+                    );
+                    const { errors, data } = await mutate({
+                        mutation: MEMBER_UPDATE,
+                        variables: { id: m.id, roles: ['leader', 'designer'] },
+                    });
+                    return { memId: m.id, errors, data };
+                });
+                expect(errors).to.be.undefined;
+                expect(data.projectMemberUpdateRoles.id).to.equal(memId);
+                expect(data.projectMemberUpdateRoles.roles).to.have.members(['designer', 'leader']);
+            });
+
+            it('should not update member roles without "member.update"', async function() {
+                const { errors, data } = await pool.transaction(async db => {
+                    const m = await memberDao.create(db, {
+                        projectId: projects.desk.id,
+                        userId: users.alphonse.id,
+                        roles: ['user', 'designer', 'some other role'],
+                    });
+                    const { mutate } = buildGqlServer(
+                        db,
+                        permsAuth(users.gerard, ['member.read', 'project.read', 'user.read'])
+                    );
+                    return await mutate({
+                        mutation: MEMBER_UPDATE,
+                        variables: { id: m.id, roles: ['leader', 'designer'] },
+                    });
+                });
+                expect(errors)
+                    .to.be.an('array')
+                    .with.lengthOf.at.least(1);
+                expect(data).to.be.null;
+            });
+        });
+
+        describe('Delete', function() {
+            it('should delete a member with "member.delete"', async function() {
+                const { mem, errors, data } = await pool.transaction(async db => {
+                    const m = await memberDao.create(db, {
+                        projectId: projects.desk.id,
+                        userId: users.alphonse.id,
+                        roles: ['user', 'designer', 'some other role'],
+                    });
+                    const { mutate } = buildGqlServer(
+                        db,
+                        permsAuth(users.gerard, [
+                            'member.delete',
+                            'member.read',
+                            'project.read',
+                            'user.read',
+                        ])
+                    );
+                    const { errors, data } = await mutate({
+                        mutation: MEMBER_DELETE,
+                        variables: { id: m.id },
+                    });
+                    return { mem: await memberDao.byId(db, m.id), errors, data };
+                });
+                expect(errors).to.be.undefined;
+                expect(data).to.be.an('object');
+                expect(data.projectMemberDelete).to.be.an('object');
+                expect(mem).to.be.null;
+            });
+
+            it('should not delete a member without "member.delete"', async function() {
+                const { errors, data } = await pool.transaction(async db => {
+                    const m = await memberDao.create(db, {
+                        projectId: projects.desk.id,
+                        userId: users.alphonse.id,
+                        roles: ['user', 'designer', 'some other role'],
+                    });
+                    const { mutate } = buildGqlServer(
+                        db,
+                        permsAuth(users.gerard, ['member.read', 'project.read', 'user.read'])
+                    );
+                    return await mutate({
+                        mutation: MEMBER_DELETE,
+                        variables: { id: m.id },
+                    });
+                });
+                expect(errors)
+                    .to.be.an('array')
+                    .with.lengthOf.at.least(1);
+                expect(data).to.be.null;
+            });
         });
     });
 });
