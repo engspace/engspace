fdc62681e6b4a5e64dd6fa62e6446c6296fbca12
Remi Thebault <remi.thebault@gmail.com>
2020-02-25 15:46:45 +0100

    testing sha1 utility

---
+++ packages/server-api/test/util.ts
@@ -0,0 +1,28 @@
+import fs from 'fs';
+import path from 'path';
+import { storePath } from '.';
+import { expect } from 'chai';
+import { bufferSha1sum, fileSha1sum } from '../src/util';
+
+describe('Util', function() {
+    describe('sha1 utility', function() {
+        // using store path as temporary location
+        const filepath = path.join(storePath, 'abcd.file');
+        const content = 'abcd';
+        const sha1 = '81fe8bfe87576c3ecb22426f8e57847382917acf';
+        before('create file', async function() {
+            await fs.promises.writeFile(filepath, content);
+        });
+        after('delete file', async function() {
+            await fs.promises.unlink(filepath);
+        });
+
+        it('should compute sha1sum of a buffer', async function() {
+            expect(bufferSha1sum(Buffer.from(content))).to.equal(sha1);
+        });
+
+        it('should compute sha1sum of a file', async function() {
+            await expect(fileSha1sum(filepath)).to.eventually.equal(sha1);
+        });
+    });
+});
