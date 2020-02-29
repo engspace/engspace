4b59b56a19f1e6aec6feac070845c7029ea19a20
Remi Thebault <remi.thebault@gmail.com>
2020-02-29 14:34:17 +0100

    adding test case

--- packages/server-db/test/util.ts
+++ packages/server-db/test/util.ts
@@ -5,5 +5,6 @@ import { identJsToSql } from '../src/util';
 describe('Util', () => {
     it('should convert Js identifier to SQL identifer', () => {
         expect(identJsToSql('fullName')).to.equal('full_name');
+        expect(identJsToSql('weirDField')).to.equal('weir_d_field');
     });
 });
