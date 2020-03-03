9553ccb741dcfe2fbb7f6fa33479aa8d3efb1cfa
Remi Thebault <remi.thebault@gmail.com>
2020-03-03 21:30:02 +0100

    CharIterator

--- packages/server-api/src/util.ts
+++ packages/server-api/src/util.ts
@@ -23,3 +23,68 @@ export function bufferSha1sum(content: Buffer): string {
     hasher.update(content);
     return hasher.digest('hex');
 }
+
+export class CharIterator {
+    private _pos = 0;
+    constructor(public readonly input: string) {}
+
+    get peek(): string {
+        if (this.done) {
+            throw new Error('cannot peek on done interator');
+        }
+        return this.input[this._pos];
+    }
+
+    get done(): boolean {
+        return this._pos >= this.input.length;
+    }
+
+    get pos(): number {
+        return this._pos;
+    }
+
+    next(): string {
+        if (this.done) {
+            throw new Error('cannot call next on done iterator');
+        }
+        const c = this.input[this._pos];
+        this._pos++;
+        return c;
+    }
+
+    back(): void {
+        if (this._pos <= 0) {
+            throw new Error('cannot go back');
+        }
+        this._pos--;
+    }
+
+    lookAhead(n: number): string {
+        if (n > this.input.length - this._pos) {
+            n = this.input.length - this._pos;
+        }
+        return this.input.slice(this._pos, this._pos + n);
+    }
+
+    read(n: number): string {
+        if (n > this.input.length - this._pos) {
+            n = this.input.length - this._pos;
+        }
+        const r = this.input.slice(this._pos, this._pos + n);
+        this._pos += n;
+        return r;
+    }
+
+    readUntil(s: string, { throws, including } = { throws: true, including: true }): string {
+        const ind = this.input.indexOf(s, this._pos);
+        const start = this._pos;
+        if (ind === -1 && throws) {
+            throw new Error(`"${s}" not found in ${this.input.slice(start)}`);
+        } else if (ind === -1) {
+            this._pos = this.input.length;
+            return this.input.slice(start);
+        }
+        this._pos = including ? ind + s.length : ind;
+        return this.input.slice(start, this._pos);
+    }
+}
--- packages/server-api/test/util.ts
+++ packages/server-api/test/util.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import { storePath } from '.';
 import { expect } from 'chai';
-import { bufferSha1sum, fileSha1sum } from '../src/util';
+import { bufferSha1sum, fileSha1sum, CharIterator } from '../src/util';
 
 describe('Util', function() {
     describe('sha1 utility', function() {
@@ -23,6 +23,118 @@ describe('Util', function() {
 
         it('should compute sha1sum of a file', async function() {
             await expect(fileSha1sum(filepath)).to.eventually.equal(sha1);
+        });
+    });
+
+    describe('CharIterator', function() {
+        it('gets next char until done', function() {
+            const inp = new CharIterator('abc');
+            expect(inp.done).to.be.false;
+            expect(inp.next()).to.equal('a');
+            expect(inp.done).to.be.false;
+            expect(inp.next()).to.equal('b');
+            expect(inp.done).to.be.false;
+            expect(inp.next()).to.equal('c');
+            expect(inp.done).to.be.true;
+        });
+        it('throws when getting char on done iterator', function() {
+            const inp = new CharIterator('');
+            expect(inp.done).to.be.true;
+            function bad(): string {
+                return inp.next();
+            }
+            expect(bad).to.throw();
+        });
+        it('peeks current char', function() {
+            const inp = new CharIterator('abc');
+            expect(inp.peek).to.equal('a');
+            expect(inp.peek).to.equal('a');
+            expect(inp.peek).to.equal('a');
+            expect(inp.next()).to.equal('a');
+            expect(inp.peek).to.equal('b');
+            expect(inp.peek).to.equal('b');
+        });
+        it('throws if peeking done iterator', function() {
+            const inp = new CharIterator('');
+            function bad(): string {
+                return inp.peek;
+            }
+            expect(bad).to.throw();
+        });
+        it('gives current char pos', function() {
+            const inp = new CharIterator('abc');
+            expect(inp.pos).to.equal(0);
+            expect(inp.next()).to.equal('a');
+            expect(inp.pos).to.equal(1);
+            expect(inp.next()).to.equal('b');
+            expect(inp.pos).to.equal(2);
+            expect(inp.next()).to.equal('c');
+            expect(inp.pos).to.equal(3);
+        });
+        it('looks ahead', function() {
+            const inp = new CharIterator('abc');
+            expect(inp.lookAhead(2)).to.equal('ab');
+            expect(inp.pos).to.equal(0);
+        });
+        it('trims when looks ahead past the end', function() {
+            const inp = new CharIterator('abc');
+            inp.next();
+            inp.next();
+            expect(inp.lookAhead(2)).to.equal('c');
+            expect(inp.pos).to.equal(2);
+        });
+        it('reads N characters', function() {
+            const inp = new CharIterator('abc');
+            expect(inp.read(2)).to.equal('ab');
+            expect(inp.pos).to.equal(2);
+        });
+        it('trims when read N characters past the end', function() {
+            const inp = new CharIterator('abc');
+            inp.next();
+            inp.next();
+            expect(inp.read(2)).to.equal('c');
+            expect(inp.done).to.be.true;
+        });
+        it('it reads until', function() {
+            const inp = new CharIterator('abcdef');
+            expect(inp.readUntil('e', { throws: false, including: false })).to.equal('abcd');
+            expect(inp.pos).to.equal(4);
+        });
+        it('it reads until and include target', function() {
+            const inp = new CharIterator('abcdef');
+            expect(inp.readUntil('e', { throws: false, including: true })).to.equal('abcde');
+            expect(inp.pos).to.equal(5);
+        });
+        it('it reads until end', function() {
+            const inp = new CharIterator('abcdef');
+            expect(inp.readUntil('z', { throws: false, including: false })).to.equal('abcdef');
+            expect(inp.done).to.be.true;
+        });
+        it('it reads until and throws', function() {
+            const inp = new CharIterator('abcdef');
+            function bad(): string {
+                return inp.readUntil('z', { throws: true, including: false });
+            }
+            expect(bad).to.throw();
+            expect(inp.pos).to.equal(0);
+        });
+        it('can go backward', function() {
+            const inp = new CharIterator('abc');
+            expect(inp.read(2)).to.equal('ab');
+            inp.back();
+            expect(inp.peek).to.equal('b');
+            inp.back();
+            expect(inp.peek).to.equal('a');
+        });
+        it('throws if going backward before begin', function() {
+            const inp = new CharIterator('abc');
+            function bad(): void {
+                inp.back();
+            }
+            expect(inp.read(1)).to.equal('a');
+            inp.back();
+            expect(inp.peek).to.equal('a');
+            expect(bad).to.throw();
         });
     });
 });
